e530485bfadff00b7fb1d8aef3ef55d4f27f19d0
Diogo Busto <diogobusto@hotmail.com>
2024-01-29 20:00:11 -0300

    Adicionando App de Enviar Emails Automaticos

---
+++ Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace EnvioEmailsAutomatico
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label = new System.Windows.Forms.Label();
+            this.txtEmailDestinatario = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btnEnviar = new System.Windows.Forms.Button();
+            this.txtEmailRemetente = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtSenhaRemetente = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtAssuntoEmail = new System.Windows.Forms.TextBox();
+            this.txtConteudoEmail = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label
+            // 
+            this.label.AutoSize = true;
+            this.label.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label.Location = new System.Drawing.Point(12, 186);
+            this.label.Name = "label";
+            this.label.Size = new System.Drawing.Size(178, 13);
+            this.label.TabIndex = 0;
+            this.label.Text = "Digite o e-mail que irá receber";
+            this.label.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtEmailDestinatario
+            // 
+            this.txtEmailDestinatario.Location = new System.Drawing.Point(12, 202);
+            this.txtEmailDestinatario.Name = "txtEmailDestinatario";
+            this.txtEmailDestinatario.Size = new System.Drawing.Size(229, 20);
+            this.txtEmailDestinatario.TabIndex = 1;
+            this.txtEmailDestinatario.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 285);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(52, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Assunto";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnEnviar
+            // 
+            this.btnEnviar.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEnviar.Location = new System.Drawing.Point(608, 578);
+            this.btnEnviar.Name = "btnEnviar";
+            this.btnEnviar.Size = new System.Drawing.Size(149, 34);
+            this.btnEnviar.TabIndex = 6;
+            this.btnEnviar.Text = "Enviar E-mail";
+            this.btnEnviar.UseVisualStyleBackColor = true;
+            this.btnEnviar.Click += new System.EventHandler(this.btnEnviar_Click);
+            // 
+            // txtEmailRemetente
+            // 
+            this.txtEmailRemetente.Location = new System.Drawing.Point(12, 65);
+            this.txtEmailRemetente.Name = "txtEmailRemetente";
+            this.txtEmailRemetente.Size = new System.Drawing.Size(229, 20);
+            this.txtEmailRemetente.TabIndex = 8;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(12, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "E-mail";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(12, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(68, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Remetente";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(11, 162);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(75, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Destinatário";
+            // 
+            // txtSenhaRemetente
+            // 
+            this.txtSenhaRemetente.Location = new System.Drawing.Point(12, 116);
+            this.txtSenhaRemetente.Name = "txtSenhaRemetente";
+            this.txtSenhaRemetente.Size = new System.Drawing.Size(229, 20);
+            this.txtSenhaRemetente.TabIndex = 12;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(12, 100);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(43, 13);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Senha";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 343);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 13;
+            this.label1.Text = "Conteúdo";
+            // 
+            // txtAssuntoEmail
+            // 
+            this.txtAssuntoEmail.Location = new System.Drawing.Point(12, 301);
+            this.txtAssuntoEmail.Name = "txtAssuntoEmail";
+            this.txtAssuntoEmail.Size = new System.Drawing.Size(229, 20);
+            this.txtAssuntoEmail.TabIndex = 14;
+            this.txtAssuntoEmail.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // txtConteudoEmail
+            // 
+            this.txtConteudoEmail.Location = new System.Drawing.Point(15, 359);
+            this.txtConteudoEmail.Multiline = true;
+            this.txtConteudoEmail.Name = "txtConteudoEmail";
+            this.txtConteudoEmail.Size = new System.Drawing.Size(742, 213);
+            this.txtConteudoEmail.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(769, 624);
+            this.Controls.Add(this.txtConteudoEmail);
+            this.Controls.Add(this.txtAssuntoEmail);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtSenhaRemetente);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtEmailRemetente);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btnEnviar);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtEmailDestinatario);
+            this.Controls.Add(this.label);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Enviar E-mail";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label;
+        private System.Windows.Forms.TextBox txtEmailDestinatario;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btnEnviar;
+        private System.Windows.Forms.TextBox txtEmailRemetente;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtSenhaRemetente;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtAssuntoEmail;
+        private System.Windows.Forms.TextBox txtConteudoEmail;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EnvioEmailsAutomatico
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnEnviar_Click(object sender, EventArgs e)
+        {
+            // Evitar múltiplos cliques enquanto a aplicação está em andamento
+            btnEnviar.Enabled = false;
+
+            // Acesso remetente
+            string emailRemetente = txtEmailRemetente.Text;
+            string senhaRemetente = txtSenhaRemetente.Text;
+
+            // E-mail destinatário
+            string emailDestinatario = txtEmailDestinatario.Text;
+
+            // Assunto
+            string emailAssunto = txtAssuntoEmail.Text;
+
+            // Mensagem
+            string emailMensagem = txtConteudoEmail.Text;
+
+            MailMessage mail = new MailMessage();
+
+            mail.From = new MailAddress(emailRemetente);
+            mail.To.Add(emailDestinatario); // PARA
+            mail.Subject = emailAssunto; //ASSUNTO
+            mail.Body = emailMensagem; // CORPO DO E-MAIL
+
+            using (var smtp = new SmtpClient("smtp-mail.outlook.com", 587))
+            {
+                smtp.UseDefaultCredentials = false;
+                smtp.EnableSsl = true;
+                smtp.Credentials = new NetworkCredential(emailRemetente, senhaRemetente);
+
+                try
+                {
+                    smtp.Send(mail);
+                    MessageBox.Show("E-mail enviado!");
+                    btnEnviar.Enabled = true;
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                    btnEnviar.Enabled = true;
+                }
+
+            }
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
+
